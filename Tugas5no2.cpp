fed2d0b2cf1f96090fb9f1a9aab8d643bddeae81
Faqih-0047 <113483163+Faqih-0047@users.noreply.github.com>
2022-10-12 20:49:26 +0700

    Add files via upload

---
+++ Tugas5no2.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  cout << "Contoh Program Perulangan" << endl;
+  for(int i=11; i<21; i++){
+    cout << i << endl;
+    }
+  cout << "\n";
+
+
+  cout << "Perulangan Menurun" << endl;
+  for(int i=20; i>10; i--){
+    cout << i << endl;
+    }
+  cout << "\n";
+
+}
