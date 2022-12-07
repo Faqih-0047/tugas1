5c837fa2a6eab2ebca3828353527a76434f668de
Faqih-0047 <113483163+Faqih-0047@users.noreply.github.com>
2022-12-07 18:43:30 +0700

    Add files via upload

---
+++ TUGAS9NO2.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+
+void swap();
+int main()
+ {
+   int a, b;
+   a = 5;
+   b = 9;
+
+   cout<<"Nilai Sebelum Fungsi Digunakan " << endl;
+   cout<<" a =  5  b = 9 "<< endl;
+   
+   swap(a,b);
+   cout<<"Nilai di dalam fungsi tambah()" << endl;
+   cout<<" m = 10  b = 16 "<< endl;
+
+   cout<<"Nilai Sebelum Fungsi Digunakan " << endl;
+   cout<<" a =  5  b = 9 "<< endl;
+
+ 
+
+ }
